163c513acb7fe0b32e41fd23058584bc5a109c98
farzan <mohammedfarzan04@gmail.com>
2025-11-07 19:22:24 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+"use client";
+import Head from 'next/head'
+import { useState } from "react";
+import Screen1 from "../components/Screen1";
+import Screen2 from "../components/Screen2";
+import Screen3 from "../components/Screen3";
+import Screen4 from "../components/Screen4";
+
+
+
+export default function Home() {
+  const [screen, setScreen] = useState(1);
+  
+  return (
+    
+    <>
+    <Head>
+    <title>Something Special🌝 4some1spcl</title>
+    <meta name="description" content="A beautiful project by ProgrammingWithFaizu" />
+  </Head>
+      {screen === 1 && <Screen1 onNext={() => setScreen(2)} />}
+      {screen === 2 && <Screen2 onNext={() => setScreen(3)} />}
+      {screen === 3 && <Screen3 onFinish={() => setScreen(4)} />}
+      {screen === 4 && <Screen4 />}
+      <div className="fixed bottom-4 right-4 text-sm text-white/40 pointer-events-none z-50">
+  @programmingwithfarzan
+  ©allrightsreservedbyfarzan
+</div>
+
+    </>
+  );
+}
